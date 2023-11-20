1d35a488fdec9c42072a03c2119491d81267279b
Léa Sel <leasel@MacBook-Pro-de-Lea.local>
2023-11-20 10:31:51 +0100

    Création README.md

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+ /**
+  * _printf - function that produces output according to a format.
+  * @format: format character string to check
+  * Description: replica of the printf function of the standard library
+  * Return: number of characters printed excluding the null byte
+  */
+
+ int _printf(const char *format, ...)
+ {
+    
+ }
