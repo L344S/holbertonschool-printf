c4229baf5cac169074bbb0216899bdc44b86bb5c
L344S <7474@holbertonstudents.com>
2023-11-21 04:57:12 -0800

    Update betty code style

--- _printf.c
+++ _printf.c
@@ -9,71 +9,73 @@
 
 int _printf(const char *format, ...)
 {
-   /* création de la variable index nécessaire à notre boucle*/
-   int index;
-   /* création de la variable qui va contenir le nombre de caractère imprimer*/
-   int strFormat_leng = 0;
+	/* création de la variable index nécessaire à notre boucle*/
+	int index;
+	/* création de la variable qui va contenir le nombre de caractère imprimer*/
+	int strFormat_leng = 0;
 
-   /* création de la variable qui va contenir notre liste d'arguments */
-   va_list arguments_list;
+	/* création de la variable qui va contenir notre liste d'arguments */
+	va_list arguments_list;
 
-   /* Vérifier si le format est vide ou ne contient pas de '%' */
-   /* si la format string est null alors on s'arrête là */
-   if (format == NULL || format[0] == '\0')
-      return (-1);
+	/* Vérifier si le format est vide ou ne contient pas de '%' */
+	/* si la format string est null alors on s'arrête là */
+	if (format == NULL || format[0] == '\0')
+		return (-1);
 
-   /* on vient remplir notre variable avec les arguments */
-   va_start(arguments_list, format);
+	/* on vient remplir notre variable avec les arguments */
+	va_start(arguments_list, format);
 
-   /* on va faire une boucle pour parcourir toute le caractère de format*/
-   for (index = 0; format[index] != '\0'; index++)
-   {
-      /* si format[index] n'est pas '%' alors on print les caractère dans stdout */
-      if (format[index] != '%')
-      {
-         /* utlisation de la fonction write pour display le texte dans stdout*/
-         write(1, &format[index], 1);
-         /* on incrémente notre variable pour garder le nombre de charactère imprimer*/
-         strFormat_leng++;
-      } 
-      else 
-      {
-         /* si la string contient % alors on regarde le caractère qui le suit*/
-         /* pour connaître la marche à suivre et adapter la suite au formay specifier*/
-         index++;
+	/* on va faire une boucle pour parcourir toute le caractère de format*/
+	for (index = 0; format[index] != '\0'; index++)
+	{
+		/* s'il n'y a aucun format specifier alors on print*/
+		if (format[index] != '%')
+		{
+			/* utlisation de la fonction write pour display le texte dans stdout*/
+			write(1, &format[index], 1);
+			/* on incrémente notre variable qui conmpte le nb de char*/
+			strFormat_leng++;
+		}
+		else
+		{
+			/* si la string contient % alors on regarde le caractère qui le suit*/
+			/* et on adapter la suite au format specifier*/
+			index++;
 
-         /* si le caractère qui suit le % est un c*/
-         if (format[index] == 'c')
-         {
-            /* on récupère la valeur en argument attribué à c et on la print*/
-            char c = va_arg(arguments_list, int);
-            write(1, &c, 1);
-            /* on oublie pas d'incrémenté la variable qui compte de caractère imprimer*/
-            strFormat_leng++;
-         }
-          /* si le caractère qui suit le % est un c*/
-         else if (format[index] == 's') 
-         {
-            /* on récupère la valeur en argument attribué à s et on compte sa longueur avec strlen*/
-            char *string = va_arg(arguments_list, char*);
-             int len_string = strlen(string);
-             /* on print la string à la place du format specifier*/
-             write(1, string, len_string);
-             /* on incrémente notre variable en y ajoutant la longueur de la string*/
-             strFormat_leng = strFormat_leng + len_string;
-         } 
-         else if (format[index] == '%') 
-         {
-            /* utlisation de la fonction write pour display le texte dans stdout*/
-            write(1, &format[index], 1);
-            /* on incrémente notre variable pour garder le nombre de charactère imprimer*/
-            strFormat_leng++;
-         }
-      }
-   }
-   /* clean up de va_list*/
-   va_end(arguments_list);
+			/* si le caractère qui suit le % est un c*/
+			if (format[index] == 'c')
+			{
+				/* on récupère la valeur en argument attribué à c et on la print*/
+				char c = va_arg(arguments_list, int);
 
-   /*on return comme demandé la nombre de caractère imprimer*/
-   return (strFormat_leng); 
+				write(1, &c, 1);
+				/* on incrémente le nombre de charactère imprimés*/
+				strFormat_leng++;
+			}
+			/* si le caractère qui suit le % est un c*/
+			else if (format[index] == 's')
+			{
+				/* on récupère la valeur en argument attribué à s*/
+				char *string = va_arg(arguments_list, char*);
+
+				int len_string = strlen(string);
+				/* on print la string à la place du format specifier*/
+				write(1, string, len_string);
+				/* on incrémente notre variable en y ajoutant la longueur de la string*/
+				strFormat_leng = strFormat_leng + len_string;
+			}
+			else if (format[index] == '%')
+			{
+				/* utlisation de la fonction write pour display le texte dans stdout*/
+				write(1, &format[index], 1);
+				/* on incrémente le nombre de charactère imprimés*/
+				strFormat_leng++;
+			}
+		}
+	}
+	/* clean up de va_list*/
+	va_end(arguments_list);
+
+	/*on return comme demandé la nombre de caractère imprimer*/
+	return (strFormat_leng);
 }
