d01a45154d1d1364d2931ebd8885529c4176e976
maelpseudo <mael.cuny@gmail.com>
2023-11-23 11:50:08 +0100

    Attempt to fix the code, betty, and syntax

--- handle_format.c
+++ handle_format.c
@@ -8,29 +8,29 @@
  */
 int handle_format(char format, va_list arguments_list)
 {
-	int strFormat_leng = 0;
+int strFormat_leng = 0;
 
-	if (format == 'c')
-	{
-		strFormat_leng = strFormat_leng + handle_character(arguments_list);
-	}
-	else if (format == 's')
-	{
-		strFormat_leng = strFormat_leng + handle_string(arguments_list);
-	}
-	else if (format == '%')
-	{
-		strFormat_leng = strFormat_leng + handle_percent();
-	}
-	else if (format == 'd' || format == 'i')
-	{
-		strFormat_leng = strFormat_leng + handle_integer(arguments_list);
-	}
-	else
-    {
-        write(1, &format, 1);
-        return (0);
-    }
+if (format == 'c')
+{
+strFormat_leng = strFormat_leng + handle_character(arguments_list);
+}
+else if (format == 's')
+{
+strFormat_leng = strFormat_leng + handle_string(arguments_list);
+}
+else if (format == '%')
+{
+strFormat_leng = strFormat_leng + handle_percent();
+}
+else if (format == 'd' || format == 'i')
+{
+strFormat_leng = strFormat_leng + handle_integer(arguments_list);
+}
+else
+{
+write(1, &format, 1);
+return (0);
+}
 
-	return (strFormat_leng);
+return (strFormat_leng);
 }
